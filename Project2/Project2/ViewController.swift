ba4680fdc352dbcb090aae38c9c025dc7d9af339
naudren <nicolas.audren@gmail.com>
2016-02-15 21:13:07 +0000

    Finished project 2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Nicolas Audren on 15/02/2016.
+//  Copyright © 2016 Nicosoft. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        askQuestion()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func askQuestion() {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        
+        title = countries[correctAnswer].uppercaseString
+    }
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        if sender.tag == correctAnswer {
+            score++
+        }
+        else{
+            score--
+            if score < 0 {
+                score = 0
+            }
+        }
+        
+        scoreLabel.text = "Your score is \(score)"
+        
+        askQuestion()
+    }
+}
+
