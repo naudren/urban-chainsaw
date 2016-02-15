775d3361bec344401d872a8abf8a108e5444b019
naudren <nicolas.audren@gmail.com>
2016-02-15 15:53:59 +0000

    Finished Project1

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Nicolas Audren on 15/02/2016.
+//  Copyright © 2016 Nicosoft. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+                title = detail
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
+
