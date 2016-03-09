0d548d4e3233cc4245bdca21185d8d8fd72bafd0
naudren <nicolas.audren@gmail.com>
2016-03-09 16:33:24 +0000

    Finished Project 7

--- Project7/Project7/DetailViewController.swift
+++ Project7/Project7/DetailViewController.swift
@@ -1,45 +1,43 @@
-//
-//  DetailViewController.swift
-//  Project7
-//
-//  Created by Nicolas Audren on 09/03/2016.
-//  Copyright © 2016 Nicosoft. All rights reserved.
-//
-
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
     }
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+        
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"], title = detailItem["title"], sigs = detailItem["sigs"], sigsNeeded = detailItem["sigsNeeded"]{
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style>"
+            html += "header {text-align:center;padding:10px;}"
+            html += "section { font-size: 150%; padding:30px}"
+            html += "footer { text-align:center; font-size: 200%; color: blue}"
+            html += "</style>"
+            html += "</head>"
+            html += "<body>"
+            html += "<header><h1>"
+            html += title
+            html += "</h1></header>"
+            html += "<section><p>"
+            html += body
+            html += "</p></section>"
+            html += "<footer>"
+            html += "Signatures: \(sigs)<br>Signatures Needed: \(sigsNeeded)"
+            html += "</footer>"
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
--- Project7/Project7/MasterViewController.swift
+++ Project7/Project7/MasterViewController.swift
@@ -11,20 +11,41 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var objects = [[String: String]]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        
+        let urlString : String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
+        
+        if let url = NSURL(string: urlString) {
+            if let data = try? NSData(contentsOfURL: url, options: []) {
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    // we're OK to parse!
+                    parseJSON(json)
+                }
+                else {
+                    showError()
+                }
+            }
+            else {
+                showError()
+            }
+        }
+        else {
+            showError()
+        }
+        
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -37,22 +58,24 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
+    
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = objects[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                controller.navigationItem.title = object["title"]
             }
         }
     }
@@ -70,24 +93,74 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"]
         return cell
     }
 
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - JSON
+    
+    //this is an example of the JSON we will get
+    
+//    {
+//      "metadata":{
+//          "responseInfo":{
+//          "status":200,
+//          "developerMessage":"OK",
+//          }
+//      },
+//      "results":[
+//          {
+//              "title":"Legal immigrants should get freedom before undocumented immigrants – moral, just and fair",
+//              "body":"I am petitioning President Obama's Administration to take a humane view of the plight of legal immigrants. Specifically, legal immigrants in Employment Based (EB) category. I believe, such immigrants were short changed in the recently announced reforms via Executive Action (EA), which was otherwise long due and a welcome announcement.",
+//              "issues":[
+//                  {
+//                      "id":"28",
+//                      "name":"Human Rights"
+//                  },
+//                  {
+//                      "id":"29",
+//                      "name":"Immigration"
+//                  }
+//              ],
+//              "signatureThreshold":100000,
+//              "signatureCount":267,
+//              "signaturesNeeded":99733,
+//          },
+//          {
+//              "title":"National database for police shootings.",
+//              "body":"There is no reliable national data on how many people are shot by police officers each year. In signing this petition, I am urging the President to bring an end to this absence of visibility by creating a federally controlled, publicly accessible database of officer-involved shootings.",
+//              "issues":[
+//                  {
+//                      "id":"28",
+//                      "name":"Human Rights"
+//                  }
+//               ],
+//              "signatureThreshold":100000,
+//              "signatureCount":17453,
+//              "signaturesNeeded":82547,
+//          }
+//          ]
+//    }
+    
+    func parseJSON(json: JSON) {
+        
+        let results = json["results"]
+        
+        for result in results.arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let sigsNeeded = result["signaturesNeeded"].stringValue
+            let obj = ["title": title, "body": body,"sigs": sigs, "sigsNeeded": sigsNeeded]
+            objects.append(obj)
+        }
+        
+        tableView.reloadData()
     }
 
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
+    
 
 
 }
